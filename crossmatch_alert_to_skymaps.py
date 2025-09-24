91de440c0b730cf5f93e24c0f6cc3ad5f2251451
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-24 13:21:56 -0500

    refactor crossmatch_alert_to_skymaps to implement fallback function for date handling and improve clarity.

--- crossmatch_alert_to_skymaps.py
+++ crossmatch_alert_to_skymaps.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from api import SkyPortal
 from utils import get_skymaps, get_valid_obj, is_obj_in_skymaps
+from astropy.time import Time
 
 load_dotenv()
 
@@ -13,12 +14,23 @@ skyportal_api_key = os.getenv("SKYPORTAL_API_KEY")
 allocation_id = os.getenv("ALLOCATION_ID")
 group_ids_to_listen = os.getenv("GROUP_IDS_TO_LISTEN")
 
-fallback_in_days = 2
+GCN = 48  # hours for GCN fallback
+ALERT = 2  # hours for alert fallback
+FIRST_DETECTION = 48  # hours for first detection fallback
+
+def fallback(hours=0, date_format=None):
+    date = datetime.utcnow() - timedelta(hours=hours)
+    if date_format == "iso":
+        return date.isoformat()
+    if date_format == "mjd":
+        return Time(date).mjd
+    return date
+
 
 def crossmatch_alert_to_skymaps():
     skyportal = SkyPortal(instance=skyportal_url, token=skyportal_api_key)
-    latest_gcn_date_obs = datetime.utcnow() - timedelta(days=fallback_in_days)
-    latest_obj_refresh = datetime.utcnow() - timedelta(hours=2)
+    latest_gcn_date_obs = fallback(GCN)
+    latest_obj_refresh = fallback(ALERT)
     cumulative_probability = 0.95
     snr_threshold = 5.0
     skymaps = None
@@ -27,32 +39,39 @@ def crossmatch_alert_to_skymaps():
         # Check if new GCNs have been observed since the last observation
         new_latest_gcn_events = skyportal.get_gcn_events(latest_gcn_date_obs + timedelta(seconds=1))
 
-        if new_latest_gcn_events: # If new GCNs, fetch again skymaps from the last fallback_in_days
+        if new_latest_gcn_events: # If new GCNs, fetch again skymaps from the GCN fallback
             print(f"New GCNs found, fetching skymaps")
             start_time = time.time()
-            skymaps = get_skymaps(skyportal, cumulative_probability, fallback_in_days)
+            skymaps = get_skymaps(skyportal, cumulative_probability, fallback(GCN))
             print(f"Fetching {len(skymaps)} skymaps and creating MOCs took {time.time() - start_time:.2f} seconds")
             latest_gcn_date_obs = datetime.fromisoformat(new_latest_gcn_events[0].get('dateobs'))
 
-        elif skymaps: # If no new GCNs, check for expired localizations and remove them
-            for dateobs, moc in skymaps.copy():
-                if dateobs >= datetime.utcnow() - timedelta(days=fallback_in_days):
+        # If no new GCNs, check for expired localizations and remove them
+        elif skymaps:
+            gcn_fallback_iso = fallback(GCN, "iso")
+            # Iterate in reverse to get older items first
+            for dateobs, moc in reversed(skymaps.copy()):
+                if dateobs >= gcn_fallback_iso:
                     break
-                else:
-                    print(f"Removed expired skymap {dateobs}")
-                    skymaps.remove((dateobs, moc))
+                print(f"Removed expired localization {dateobs}")
+                skymaps.remove((dateobs, moc))
 
         # Retrieve objects created after last refresh time
         if skymaps:
             get_objects_payload = {
-                "startDate": max(latest_obj_refresh, datetime.utcnow() - timedelta(days=fallback_in_days)).isoformat(),
+                "startDate": max(latest_obj_refresh, fallback(ALERT)).isoformat(),
                 "includePhotometry": True,
             }
             if group_ids_to_listen:
                 get_objects_payload["groupIDs"] = group_ids_to_listen
 
             latest_obj_refresh=datetime.utcnow() # Update the refresh time before the query
-            objs = get_valid_obj(skyportal, get_objects_payload, snr_threshold, fallback_in_days)
+            objs = get_valid_obj(
+                skyportal,
+                get_objects_payload,
+                snr_threshold,
+                fallback(FIRST_DETECTION, "mjd")
+            )
             crossmatches = []
             start_time = time.time()
             for obj in objs:
