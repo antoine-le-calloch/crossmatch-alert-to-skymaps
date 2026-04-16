fa2a93befd874b654865d9c45a882e4c37f26c98
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-04-16 14:49:37 -0400

    Update produce_to_gcn call in heartbeat payload to disable validation.

--- gcn/produce_gcn_notices.py
+++ gcn/produce_gcn_notices.py
@@ -47,4 +47,4 @@ def produce_gcn_heartbeat():
         "timestamp": Time.now().isot + "Z",
         "status": "alive"
     }
-    produce_to_gcn(HEARTBEAT_TOPIC, heartbeat_payload)
+    produce_to_gcn(HEARTBEAT_TOPIC, heartbeat_payload, validate=False)
